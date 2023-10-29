90c91c5ff7c5fb045d51c0e8e0549672ae59f048
Aexetan01 <timcikvana27@gmail.com>
2023-10-29 10:34:23 +0200

    initiall commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1Ivan_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetSquare_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(tbA.Text, out double A) &&
+                double.TryParse(tbH.Text, out double H))
+            {
+                Trialngle trialngle = new Trialngle(A, H);
+
+                MessageBox.Show($"Square: {trialngle.GetSquare()}", "Result!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show($"Incorrect data!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnGetPerimetr_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(tbA.Text, out double A) &&
+                double.TryParse(tbB.Text, out double B) &&
+                double.TryParse(tbC.Text, out double C))
+            {
+
+                Trialngle trialngle = new Trialngle(A, B, C);
+
+                MessageBox.Show($"Perimetr: {trialngle.GetPerimetr()}", "Result!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show($"Incorrect data!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ Trialngle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Ivan_WPF
+{
+    public class Trialngle
+    {
+        private double a;
+        private double b;
+        private double c;
+        private double h;
+
+        public Trialngle()
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            h = 0;
+        }
+
+        public Trialngle(double a, double h)
+        {
+            this.a = a;
+            this.h = h;
+            b = 0;
+            c = 0;
+        }
+
+        public Trialngle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            h = 0;
+        }
+
+        public Trialngle(double a, double b, double c, double h) : this(a, b, c)
+        {
+            this.h = h;
+        }
+
+        public double A { get => a; set => a = value; }
+        public double B { get => b; set => b = value; }
+        public double C { get => c; set => c = value; }
+        public double H { get => h; set => h = value; }
+
+        public double GetPerimetr()
+        {
+            return a + b + c;
+        }
+
+        public double GetSquare()
+        {
+            return (a * h) / 2d;
+        }
+
+
+    }
+}
